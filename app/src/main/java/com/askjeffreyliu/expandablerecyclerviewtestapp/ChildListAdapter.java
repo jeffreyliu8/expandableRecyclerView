fcadda3f0bbfd708c2e19e0b2f237867c8a05049
Jeffrey Liu <jeffreyliu@gmail.com>
2021-01-06 15:28:03 -0800

    androidx update

--- app/src/main/java/com/askjeffreyliu/expandablerecyclerviewtestapp/ChildListAdapter.java
+++ app/src/main/java/com/askjeffreyliu/expandablerecyclerviewtestapp/ChildListAdapter.java
@@ -1,9 +1,9 @@
 package com.askjeffreyliu.expandablerecyclerviewtestapp;
 
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/com/askjeffreyliu/expandablerecyclerviewtestapp/MainActivity.java
+++ app/src/main/java/com/askjeffreyliu/expandablerecyclerviewtestapp/MainActivity.java
@@ -1,10 +1,10 @@
 package com.askjeffreyliu.expandablerecyclerviewtestapp;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.util.Log;
 
 import java.util.ArrayList;
